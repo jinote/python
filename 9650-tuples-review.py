5b6b51364f6e7941827c485e7a0eebd89d8f5c5d
Hojin Lee <62555770+jinote@users.noreply.github.com>
2021-04-17 19:00:48 +0900

    Create 9650-tuples-review.py

---
+++ 9650-tuples-review.py
@@ -0,0 +1,10 @@
+# tuples: immutable
+# tuple_name = (item1, item2)
+
+# Advantages
+## processing tuples is faster than processing lists
+## tuples are safe
+## some oprations in Python require use of tuples
+## list() -> converts tuple to list
+## tuple() -> converts list to tuple
+
