51125fb7734e78e90ddff1085f44e621d3daebc9
Hojin Lee <62555770+jinote@users.noreply.github.com>
2021-04-17 18:57:56 +0900

    Update and rename 9650-lists-review(1).py to 9650-lists-review.py

--- 9650-lists-review.py
+++ 9650-lists-review.py
@@ -103,3 +103,86 @@ list_b.reverse() # modified
 list_b[::-1] # it's just a copy so,
 list_b = list_b[::-1] # saved now
 -> ['sep', 'may', 'mar', ........ 'aug', 'apr'] 
+
+# Delete elements
+
+# list.remove(item) - NOT index
+list_b.remove('dec')
+-> ['apr', 'aug', 'feb', 'jan', 'jul', 'jun', 'mar', 'may', 'sep'] 
+
+list_c = [1,2,3,4]
+list_c.pop(-1)
+-> 4
+list_c
+-> [1,2,3]
+list_d = []
+list_d.append(list_c.pop(-1))
+list_d.append(list_c.pop(-1))
+list_d.append(list_c.pop(-1))
+list_c
+-> [] # empty
+list_d
+-> [3,2,1] # fill
+
+# del statement
+del list_b[-1]
+-> ['apr', 'aug', 'feb', 'jan', 'jul', 'jun', 'mar', 'may'] 
+
+# math functions for lists
+grades = [99,98,88,76]
+sum(grades)
+-> 361
+sum(grades)/len(grades)
+-> 90.25
+max(grades)-min(grades)
+-> 23
+list_b = ['jan', 'feb', 'mar', 'apr']
+list_b.sort()
+list_b
+-> ['apr', 'feb', 'jan', 'mar']
+
+
+# list comprehension
+doubles_odds = []
+for n in numbers:
+  if n % 2 == 1:
+    doubled_odds.append(n*2)
+    
+doubled_odds = [n * 2 for n in numbers if n % 2 == 1]
+
+# different filtering list problems
+old_list = [1,3,5,7,8,23,66]
+new_list = []
+for n in old_list:
+  if n > 8:
+    new_list.append(n)
+    
+print(new_list)
+
+# list comprehendsion
+old_list = [1,3,5,7,8,23,66]
+new_list = [n for in old_list if n == 8 or n == 66]
+print(new_list)
+-> [8, 66]
+
+years = [str(n) for n in range(2000,2022)]
+years
+-> ['2000', '2001', '2002', '2003', '2004'.....'2021']
+
+infile = oepn("c:\\temp\\52weekslow.txt","r")
+mylist = infile.readlines()
+
+newlist = [for n in mylist if n.startswith("A")]
+newlist
+['AA', 'AEIS', 'AMG', 'AMWD', 'AN', 'AOS'......]
+
+# two-dimensional lists
+mx = [[1,2,3],[4,5,6],[7,8,9]]
+mx[0]
+-> [1,2,3]
+mx[1]
+-> [4,5,6]
+type(max[0])
+-> <class 'list'>
+
+
